ccebc6801341befa5dbe7d40c8a60f175e9b8d94
lmartinez <lmartinezj@gmail.com>
2023-03-30 00:26:18 -0500

    back to helmet 3.21 and curly brace notation with double quotes

--- myApp.js
+++ myApp.js
@@ -5,8 +5,11 @@ const app = express();
 const ninetyDaysInSeconds = 90*24*60*60
 
 app.use(helmet.hidePoweredBy())
-app.use(helmet.frameguard(
+app.use(helmet(
+  { 
+    frameguard:
      { action: "deny" }
+  }
     /*,
     hsts: {
       maxAge: ninetyDaysInSeconds,
