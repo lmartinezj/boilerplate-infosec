fe3d3e6f630a1eaa73eaef6d6d3d0ce7c605619b
lmartinez <lmartinezj@gmail.com>
2023-03-30 00:12:32 -0500

    set double quotes in deny

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ const ninetyDaysInSeconds = 90*24*60*60
 
 app.use(helmet.hidePoweredBy())
 app.use(helmet.frameguard(
-      { action: 'deny' }
+      { action: "deny" }
     /*,
     hsts: {
       maxAge: ninetyDaysInSeconds,
