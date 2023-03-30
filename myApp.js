79441c89453e82f235c054fdda5519988de2e307
lmartinez <lmartinezj@gmail.com>
2023-03-29 23:51:02 -0500

    uppercase

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ const ninetyDaysInSeconds = 90*24*60*60
 app.use(helmet.hidePoweredBy())
 app.use(helmet.frameguard(
   
-    { action: 'deny' }
+    { action: 'DENY' }
     /*,
     hsts: {
       maxAge: ninetyDaysInSeconds,
