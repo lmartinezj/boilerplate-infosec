da92507630339071a2868d6da389cf949058f65e
lmartinez <lmartinezj@gmail.com>
2023-03-30 00:16:27 -0500

    took frameguard inside curly braces

--- myApp.js
+++ myApp.js
@@ -5,8 +5,12 @@ const app = express();
 const ninetyDaysInSeconds = 90*24*60*60
 
 app.use(helmet.hidePoweredBy())
-app.use(helmet.frameguard(
-      { action: "deny" }
+app.use(helmet(
+  {
+    frameguard: {
+      action: "deny"
+    }
+  }
     /*,
     hsts: {
       maxAge: ninetyDaysInSeconds,
