ba40038432fad9db1feb8aa9fded4b8b8884e4e6
lmartinez <lmartinezj@gmail.com>
2023-04-11 10:08:40 -0500

    Fixed typo on middleware invocation

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.use(helmet.hidePoweredBy())
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff())
 app.use(helmet.ieNoOpen())
-app.helmet(noCache())
+app.use(helmet.noCache())
 app.use(helmet.frameguard(
   { 
     action: "deny"
