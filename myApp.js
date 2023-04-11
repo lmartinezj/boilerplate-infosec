e92a171822615b767632dc66782a3a7ebad12f46
lmartinez <lmartinezj@gmail.com>
2023-04-11 09:25:38 -0500

    Avoid Inferring the Response MIME Type with helmet.noSniff()

--- myApp.js
+++ myApp.js
@@ -6,6 +6,7 @@ const ninetyDaysInSeconds = 90*24*60*60
 
 app.use(helmet.hidePoweredBy())
 app.use(helmet.xssFilter());
+app.use(helmet.noSniff())
 app.use(helmet.frameguard(
      { action: "deny" }
     /*,
